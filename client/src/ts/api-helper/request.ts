40eb4107415aa9f2a37f295f7451e7d79fc63bd0
Markaronin <jmarkroylance@gmail.com>
2021-10-29 13:24:56 -0600

    Fix edit request

--- client/src/ts/api-helper/request.ts
+++ client/src/ts/api-helper/request.ts
@@ -4,4 +4,4 @@ export type CreateBudgetRequest = Omit<Budget, "id" | "users">;
 export type UpdateBudgetRequest = Partial<Omit<Budget, "id">>;
 
 export type CreateExpenseRequest = Omit<Expense, "id">;
-export type UpdateExpenseRequest = Partial<Omit<Expense, "id">>;
+export type UpdateExpenseRequest = Omit<Expense, "id">;
